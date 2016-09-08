f44e245d99317c922d95f7fe32c9c9c186823276
Matt Hughes <mhughes27@gmail.com>
2016-09-08 10:23:48 -0500

    static site using dotnet and vs core

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("This is the beginning of a DOTNET revolution!");
+            });
+        }
+    }
+}
